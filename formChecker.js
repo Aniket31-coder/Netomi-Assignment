b49b9b469d37514196cf3e294d1de5123c0c6f6c
Aniket31-coder <aniketdewnani31@gmail.com>
2023-05-03 15:49:14 +0530

    Initial Commit

---
+++ formChecker.js
@@ -0,0 +1,104 @@
+// Using Countriesnow.space API to fetch both countries as well as states
+fetch("https://countriesnow.space/api/v0.1/countries/states")
+  .then((response) => response.json())
+  .then((data) => {
+    const countrySelect = document.getElementById("country");
+    const stateSelect = document.getElementById("state");
+
+    // Looping through each country and adding it in the dropdown menu
+    data.data.forEach((country) => {
+      const option = document.createElement("option");
+      option.value = country.name;
+      option.textContent = country.name;
+      countrySelect.appendChild(option);
+    });
+
+    // Checking for changes in the country selected, if the country is changed, we would update the states accordingly
+    countrySelect.addEventListener("change", (event) => {
+      const selectedCountry = event.target.value;
+      // Checking for the states of that particular selected country.
+      const countryData = data.data.find((c) => c.name === selectedCountry);
+      if (countryData) {
+        const states = countryData.states;
+        // console.log(countryData); Consoling the country data, which also contains the names of the states
+        stateSelect.innerHTML = ""; // Clear the previous state selection from the state field.
+        states.forEach((state) => {
+          const option = document.createElement("option");
+          option.value = state.name;
+          option.textContent = state.name;
+          stateSelect.appendChild(option);
+        });
+      }
+    });
+  })
+  .catch((error) => {
+    console.error("Error while fetching from API of countries and states", error);
+  });
+
+  function checkValidity(emailValue) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Using Regular Expression to check for validity of emails.
+    return emailRegex.test(emailValue);
+  }
+
+  function validateForm() {
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const contact = document.getElementById("contact").value.trim();
+    const country = document.getElementById("country").value;
+    const state = document.getElementById("state").value;
+
+    // Validating each value using the validators from the main window
+    const validators = parent.validators || []
+
+    //Storing errors
+    const errors = {};
+
+    console.log(validators);
+    if(Array.isArray(validators)) {
+        validators.forEach((validator) => {
+            console.log(validator);
+            const field = validator.field;
+            const value = field === "name" ? name :
+                          field === "email" ? email :
+                          field === "phone" ? contact :
+                          field === "country" ? country :
+                          field === "state" ? state : null;
+    
+            console.log(field+" "+value);
+            if (value !== null) {
+                validator.validator.forEach((rule) => {
+                    if(rule.required && !value.trim()) errors[field] = { error: "Please fill this field, it is required." };
+                    
+                    else if(rule.minlengthname && value.length < rule.minlengthname) 
+                        errors[field] = { error: `Length should be in between 4-10 characters.` };
+
+                    else if(rule.maxlengthname && value.length > rule.maxlengthname) 
+                        errors[field] = { error: `Length should be in between 4-10 characters.` };
+                    
+                    else if(rule.email && !checkValidity(email)) 
+                        errors[field] = { error: "Please enter a valid email address."};
+                    
+                    else if(rule.mobile && value.length !== rule.mobile.length) 
+                        errors[field] = { error: "Mobile number should contain 10 digits."};
+                });
+            }
+          });
+    }
+    
+    // if(name.length<4||name.length>10) errors["name"] = { error: "Length should be in between 4-10 characters" };
+    // if(!checkValidity(email)) errors["email"] = { error: "Please enter a valid email address" };
+    // if(contact.length !== 10) errors["contact number"] = { error: "Mobile number should be of 10 digits" };
+    // if(country === "") errors["country"] = { error: "Please select a country" };
+    // if(state === "") errors["state"] = { error: "Please select a state" };
+
+    if(Object.keys(errors).length === 0) {
+        const result = { Success: "All fields are valid." };
+        console.log(JSON.stringify(result));
+        parent.postMessage(JSON.stringify(result), "*");
+    } 
+    else {
+        const result = errors;
+        console.log(JSON.stringify(result));
+        parent.postMessage(JSON.stringify(result), "*");
+    }
+}  
---
+++ index.js
@@ -0,0 +1,37 @@
+var validators = [
+      {
+        "field": "name",
+        "validator": [
+            { "required": true }, 
+            { "minlengthname": 4 }, 
+            {"maxlengthname": 10 }]
+      },
+      {
+        "field": "email",
+        "validator": [{ "required": true }, { "email": true }]
+      },
+      {
+        "field": "phone",
+        "validator": [{ "required": true }, { "mobile": { "length" : 10} } ]
+      },
+      {
+        "field": "country",
+        "validator": [{ "required": true }]
+      },
+      {
+        "field": "state",
+        "validator": [{ "required": true }]
+      }
+    ];
+
+// Listening for postMessage event from the iframe and displaying the result on the screen
+window.addEventListener("message", function(event) {
+    if (event.source !== parent && event.origin !== "http://127.0.0.1:5500") return;
+    if (typeof event.data === "string") {
+        var response = JSON.parse(event.data)
+        if("Success" in response) document.getElementById("myFrame").style.borderColor = "green";
+        else document.getElementById("myFrame").style.borderColor = "red";
+        const resultDiv = document.getElementById("result");
+        resultDiv.innerText = "Result: "+ event.data;
+    }
+  });
